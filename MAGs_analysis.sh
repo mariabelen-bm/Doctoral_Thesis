7aabd3284f29f9ea44de7e0dc0c4c0a69fd417c1
María Belén Barquero Martínez <81907372+mariabelen-bm@users.noreply.github.com>
2025-01-23 11:26:13 +0100

    Create MAGs_analysis.sh

---
+++ MAGs_analysis.sh
@@ -0,0 +1,5 @@
+###################
+## MAGs ANALYSIS ##
+###################
+
+The analysis of MAGs (Metagenome-Assembled Genomes) is an approach used in metagenomics to reconstruct complete genomes of microorganisms present in an environmental sample, without the need for prior isolation in culture. Using raw metagenomic sequences, MAGs are obtained by an assembly and binning process, in which contigs (DNA fragments) are grouped into bins representing individual genomes. These genomes can come from bacteria, archaea or other microbes present in the sample. 
